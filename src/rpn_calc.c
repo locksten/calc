769dd63eeac32dab75b7233e2dd32f9578309686
locksten <locksten@users.noreply.github.com>
2014-11-27 17:09:35 +0200

    Split up exec_operator() into smaller functions

--- src/rpn_calc.c
+++ src/rpn_calc.c
@@ -7,6 +7,8 @@
 
 static int exec_operator(stack *_stack, char _operator[]);
 static int exec_constant(stack *_stack, char _operator[]);
+static int exec_1_oper(stack *_stack, char _operator[]);
+static int exec_2_oper(stack *_stack, char _operator[]);
 static int is_number(char s[]);
 
 static int exec_operator(stack *_stack, char _operator[])
@@ -18,66 +20,18 @@ static int exec_operator(stack *_stack, char _operator[])
 		fprintf(stderr, "error: too few arguments\n");
 		return -1;
 	}
-	double *answer = (double *)malloc(sizeof(double));
-	double *op1 = (double *)stack_pop(_stack);
-	if (strcmp(_operator, "sqrt") == 0)
-		*answer = sqrt(*op1);
-	else if (strcmp(_operator, "ln") == 0)
-		*answer = log(*op1);
-	else if (strcmp(_operator, "lb") == 0)
-		*answer = log2(*op1);
-	else if (strcmp(_operator, "lg") == 0)
-		*answer = log10(*op1);
-	else if (strcmp(_operator, "cos") == 0)
-		*answer = cos(*op1);
-	else if (strcmp(_operator, "sin") == 0)
-		*answer = sin(*op1);
-	else if (strcmp(_operator, "tan") == 0)
-		*answer = tan(*op1);
-	else if (strcmp(_operator, "acos") == 0)
-		*answer = acos(*op1);
-	else if (strcmp(_operator, "asin") == 0)
-		*answer = asin(*op1);
-	else if (strcmp(_operator, "atan") == 0)
-		*answer = atan(*op1);
-	else if (strcmp(_operator, "abs") == 0)
-		*answer = fabs(*op1);
-	else if (strcmp(_operator, "floor") == 0)
-		*answer = floor(*op1);
-	else if (strcmp(_operator, "ceil") == 0)
-		*answer = ceil(*op1);
-	else {
-		if (_stack->size < 1) {
-			fprintf(stderr, "error: too few arguments\n");
-			free(answer);
-			free(op1);
-			return -1;
-		}
-		double *op2 = (double *)stack_pop(_stack);
-		if (strcmp(_operator, "*") == 0)
-			*answer = *op2 * *op1;
-		else if (strcmp(_operator, "/") == 0)
-			*answer = *op2 / *op1;
-		else if (strcmp(_operator, "+") == 0)
-			*answer = *op2 + *op1;
-		else if (strcmp(_operator, "-") == 0)
-			*answer = *op2 - *op1;
-		else if (strcmp(_operator, "^") == 0)
-			*answer = pow(*op2, *op1);
-		else if (strcmp(_operator, "%") == 0)
-			*answer = fmod(*op2, *op1);
-		else {
-			fprintf(stderr, "error: unrecognized operator\n");
-			free(answer);
-			free(op1);
-			free(op2);
-			return -1;
-		}
-		free(op2);
+	if (!exec_1_oper(_stack, _operator))
+		return 0;
+
+	if (_stack->size < 2) {
+		fprintf(stderr, "error: too few arguments\n");
+		return -1;
 	}
-	stack_push(_stack, answer);
-	free(op1);
-	return 0;
+	if (!exec_2_oper(_stack, _operator))
+		return 0;
+
+	fprintf(stderr, "error: unrecognized operator\n");
+	return -1;
 }
 
 double *calculate(char *input)
@@ -139,6 +93,73 @@ static int exec_constant(stack *_stack, char _operator[]) {
 		free(answer);
 		return -1;
 	}
+	stack_push(_stack, answer);
+	return 0;
+}
+
+static int exec_1_oper(stack *_stack, char _operator[]) {
+	double *op = (double *)stack_pop(_stack);
+	double *answer = (double *)malloc(sizeof(double));
+	if (strcmp(_operator, "sqrt") == 0)
+		*answer = sqrt(*op);
+	else if (strcmp(_operator, "ln") == 0)
+		*answer = log(*op);
+	else if (strcmp(_operator, "lb") == 0)
+		*answer = log2(*op);
+	else if (strcmp(_operator, "lg") == 0)
+		*answer = log10(*op);
+	else if (strcmp(_operator, "cos") == 0)
+		*answer = cos(*op);
+	else if (strcmp(_operator, "sin") == 0)
+		*answer = sin(*op);
+	else if (strcmp(_operator, "tan") == 0)
+		*answer = tan(*op);
+	else if (strcmp(_operator, "acos") == 0)
+		*answer = acos(*op);
+	else if (strcmp(_operator, "asin") == 0)
+		*answer = asin(*op);
+	else if (strcmp(_operator, "atan") == 0)
+		*answer = atan(*op);
+	else if (strcmp(_operator, "abs") == 0)
+		*answer = fabs(*op);
+	else if (strcmp(_operator, "floor") == 0)
+		*answer = floor(*op);
+	else if (strcmp(_operator, "ceil") == 0)
+		*answer = ceil(*op);
+	else {
+		stack_push(_stack, op);
+		free(answer);
+		return -1;
+	}
+	free(op);
+	stack_push(_stack, answer);
+	return 0;
+}
+
+static int exec_2_oper(stack *_stack, char _operator[]) {
+	double *op1 = (double *)stack_pop(_stack);
+	double *op2 = (double *)stack_pop(_stack);
+	double *answer = (double *)malloc(sizeof(double));
+	if (strcmp(_operator, "*") == 0)
+		*answer = *op2 * *op1;
+	else if (strcmp(_operator, "/") == 0)
+		*answer = *op2 / *op1;
+	else if (strcmp(_operator, "+") == 0)
+		*answer = *op2 + *op1;
+	else if (strcmp(_operator, "-") == 0)
+		*answer = *op2 - *op1;
+	else if (strcmp(_operator, "^") == 0)
+		*answer = pow(*op2, *op1);
+	else if (strcmp(_operator, "%") == 0)
+		*answer = fmod(*op2, *op1);
+	else {
+		stack_push(_stack, op2);
+		stack_push(_stack, op1);
+		free(answer);
+		return -1;
+	}
+	free(op1);
+	free(op2);
 	stack_push(_stack, answer);
 	return 0;
 }
